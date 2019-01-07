359ce6075f9c070317e7968563b40569deb46200
Armaan Goel <armaangoel78@gmail.com>
2019-01-07 16:08:51 -0500

    some slight changes

--- controller.py
+++ controller.py
@@ -4,14 +4,19 @@ import pygame
 class Controller:
     def __init__(self):
         pygame.joystick.init()
-        self.joystick = ([pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())])[0]
-        self.joystick.init()
+        joysticks = ([pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())])
+        if len(joysticks) > 0:
+            self.joystick = joysticks[0]
+            self.joystick.init()
+        else: 
+            self.joystick = None
 
         self.update()
 
 
     def update(self):
-        self.value = self.joystick.get_axis(1)
+        if self.joystick != None:
+            self.value = self.joystick.get_axis(1)
 
     def up(self):
         return self.value < -0.5
--- v3.py
+++ v3.py
@@ -9,6 +9,9 @@ import threading
 import cv2
 from error import Error
 
+def func():
+    return False
+
 def face_stuff(paddle_1):
     win_w = 500
     win_h = 500
@@ -75,9 +78,9 @@ win_h = 500
 win = pygame.display.set_mode((win_w, win_h))
 pygame.display.set_caption("Wernk-Pong")
 
-controller = Controller()
-paddle_1 = Paddle(win_w, win_h, 10, "assets/wernkepaddle3.png", 10, controller.w, controller.s)
-paddle_2 = Paddle(win_w, win_h, win_w - 30, "assets/beckpaddle3.png", 7, controller.up, controller.down)
+# controller = Controller()
+paddle_1 = Paddle(win_w, win_h, 10, "assets/wernkepaddle3.png", 10, func, func)
+paddle_2 = Paddle(win_w, win_h, win_w - 30, "assets/beckpaddle3.png", 7, func, func)
 scoreboard = Scoreboard(win_w, win_h)
 ball = Ball(paddle_1, paddle_2, scoreboard, win_w, win_h, 12, "assets/wernkeball2.png",
             "assets/beckball.png")  # 15 seems to fast for the player
@@ -100,7 +103,7 @@ while run:
 
     win.fill((0, 0, 0))
 
-    controller.update()
+    # controller.update()
     if (scoreboard.player_1_score + scoreboard.player_2_score < 5):
         sprites.update()
     else:
